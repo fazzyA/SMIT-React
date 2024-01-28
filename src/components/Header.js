c2ca4b25dbee3aa4270088b38994bc6051bddc4b
Faiza Aziz Khan <faz.pak@gmail.com>
2024-01-28 12:00:35 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import Button from "./Button";
 
-export default function Header(){
+export default function Header({ heading, text }){
     return (
         <>
         <h1>AOA - React Class</h1>
-        {/* <Button /> */}
+        <h1>{heading}</h1>
+        <p>{text}</p>
+
         </>
     )
 }
