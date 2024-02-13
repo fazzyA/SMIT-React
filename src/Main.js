e0b8ae0637ba89e3f40421e89f7f294605983152
fazzyA <faz.pak@gmail.com>
2024-02-13 17:12:34 +0500

    ternary operator

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,11 @@
-import MyButton from "./components/MyButton"
-
 const Main = ({children , heading}) => {
     console.log("🚀 ~ Main ~ children:", children)
     const a = 10
+    
     return (
         <div>
             <h2>{heading}</h2>
             <b>{children ? children : <p>dummy text</p>}</b>
-            <MyButton />
             <h3>{(a > 11) ? "Login" : "Logout"}</h3>
         </div>
     )
--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,11 +1,11 @@
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 
-function MyButton({heading}) {
-  return (
-    <>
-      <Button variant="success" size='lg' >{heading ? heading : "Success"}</Button>{' '}
-    </>
-  );
-}
+// function MyButton({heading}) {
+//   return (
+//     <>
+//       <Button variant="success" size='lg' >{heading ? heading : "Success"}</Button>{' '}
+//     </>
+//   );
+// }
 
-export default MyButton;
+// export default MyButton;
